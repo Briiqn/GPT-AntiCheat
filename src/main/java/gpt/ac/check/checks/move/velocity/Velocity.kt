5a6d19d5ca68075812ae21c03ea937435aca2a84
Briiqn <82249811+Briiqn@users.noreply.github.com>
2025-03-21 10:02:23 -0400

    Update Velocity.kt
    forgot to commit

--- src/main/java/gpt/ac/check/checks/move/velocity/Velocity.kt
+++ src/main/java/gpt/ac/check/checks/move/velocity/Velocity.kt
@@ -1,5 +1,19 @@
 package gpt.ac.check.checks.move.velocity
 
 class Velocity {
-
+SpecialList<Vec3<Double>> nextposablePositons= new List()
+@EventEars
+prublkc vood onVelocitySended(Pakat VelocityPakt, Player playar){
+nextposablePositons=runSimulation(player,packet.vec)
+}
+@EventEars
+purlic vood onFlying(Pakat PoositinPakt , Player playr){
+if(pakat.positon!=nextposablePositions.next()){
+flag(player,1,"fly hack")
+}
+}
+prubcli startic SpecialList<Vec3<Double>> runSimulation(){
+//We will use bruteforce air tick simulation for 99.99% detection 
+TODO()
+}
 }
